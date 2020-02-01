960a7800f483f5fb0a2a8c2b8283e761cf767eab
Noah Manneschmidt <nmannesc@twitch.tv>
2020-02-01 11:58:39 -0800

    First commit to convert from sample to own project

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,41 @@
-const path = require('path');
+const path = require("path");
+const hwp = require("html-webpack-plugin");
+const package = require("./package.json");
 
 module.exports = {
-    entry: './src/main.ts',
-    devtool: 'source-map',
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    output: {
-        filename: '[name].js',
-        sourceMapFilename: '[file].map',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.(png|jpg|bmp|wav|mp3)$/,
-                use: [{
-                  loader: 'file-loader',
-                  options: {
-                    emitFile: true
-                  }
-                }]
-              }
+  entry: "./src/main.ts",
+  devtool: "source-map",
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  output: {
+    filename: "[name].js",
+    sourceMapFilename: "[file].map",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|bmp|wav|mp3)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              emitFile: true
+            }
+          }
         ]
-    }
-}
+      }
+    ]
+  },
+  plugins: [
+    new hwp({
+      title: package.description
+    })
+  ]
+};
