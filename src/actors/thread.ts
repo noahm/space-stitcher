7f0f3211447fd87ea3c4a6741b11c272a6745217
Noah Manneschmidt <nmannesc@twitch.tv>
2020-02-02 00:29:26 -0800

    Correctly draw anchored threads

--- src/actors/thread.ts
+++ src/actors/thread.ts
@@ -1,43 +1,42 @@
 import { Vector, Color, Actor } from "excalibur";
-
 import { Ship } from "./ship";
 
 export class SpaceThread extends Actor {
-    private startPoint: Vector;
-    private ship: Ship;
-    private endPoint: Vector | null;
-  
-    constructor(ship: Ship) {
-      super({
-        pos: ship.center,
-        width: 10,
-        height: 10
-      });
+  private startPoint: Vector;
+  private ship: Ship;
+  private endPoint: Vector | null;
 
-      this.ship = ship;
-      this.startPoint = ship.center;
-      this.endPoint = null;
+  constructor(ship: Ship) {
+    super({
+      pos: ship.center,
+      width: 10,
+      height: 10
+    });
+
+    this.ship = ship;
+    this.startPoint = ship.center;
+    this.endPoint = null;
+  }
+
+  draw(ctx: CanvasRenderingContext2D, delta: number) {
+    ctx.beginPath();
+    ctx.strokeStyle = Color.Magenta.toString();
+    ctx.lineWidth = 2;
+    ctx.lineJoin = "round";
+    ctx.lineCap = "round";
+
+    ctx.moveTo(this.startPoint.x, this.startPoint.y + this.ship.height / 2);
+    if (this.endPoint) {
+      ctx.lineTo(this.endPoint.x, this.endPoint.y);
+    } else {
+      const { x, y } = this.ship.getThreadAttachPoint();
+      ctx.lineTo(x, y);
     }
+    ctx.stroke();
+    ctx.closePath();
+  }
 
-    draw(ctx: CanvasRenderingContext2D, delta: number) {
-        ctx.beginPath();
-        ctx.strokeStyle = Color.Magenta.toString();
-        ctx.lineWidth = 2;
-        ctx.lineJoin = 'round';
-        ctx.lineCap = 'round';
-
-        ctx.moveTo(this.startPoint.x, this.startPoint.y + this.ship.height / 2);
-        if (this.endPoint) {
-            ctx.moveTo(this.endPoint.x, this.endPoint.y);
-        } else {
-            const { x, y } = this.ship.getThreadAttachPoint();
-            ctx.lineTo(x, y)
-        }
-        ctx.stroke();
-        ctx.closePath();
-    }
-
-    anchorThread() {
-        this.endPoint = this.ship.center;
-    }
+  anchorThread() {
+    this.endPoint = this.ship.center;
+  }
 }
