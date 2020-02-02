5141ca7928c1c9f22cd0b075e6b843ca90471404
Noah Manneschmidt <nmannesc@twitch.tv>
2020-02-02 08:58:40 -0800

    Convert star spawns to timer, remove baddie

--- src/rift-level.ts
+++ src/rift-level.ts
@@ -1,27 +1,25 @@
-import * as ex from "excalibur";
 import { Ship } from "./actors/ship";
-import { Baddie } from "./actors/baddie";
 
 import { animManager } from "./actors/animation-manager";
 import { addRift } from "./actors/rift";
 import { getStar } from "./actors/star";
+import { Engine, Scene, Timer } from "excalibur";
 
-export class RiftLevel extends ex.Scene {
-  onInitialize(engine: ex.Engine) {
+export class RiftLevel extends Scene {
+  onInitialize(engine: Engine) {
     engine.add(animManager);
 
     for (let i = 0; i < 180; i++) {
-      engine.add(getStar(engine, { randomX: true }))
+      engine.add(getStar(engine, { randomX: true }));
     }
 
-    setInterval(() => {
+    const starTimer = new Timer(() => {
       engine.add(getStar(engine));
     }, 400);
+    engine.add(starTimer);
 
     addRift(engine);
     const ship = new Ship(engine, engine.halfDrawWidth, 800, 50, 137);
-    const baddie = new Baddie(engine.halfDrawWidth + 200, 200, 50, 50, ship);
-    engine.add(baddie);
     engine.add(ship);
   }
 }
