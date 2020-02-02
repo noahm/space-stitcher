69694d7af547409a42be193b0df6b0af0713e19d
Ben Gordon <bgordon@twitch.tv>
2020-02-02 08:39:48 -0800

    thread colors / knots

--- src/actors/thread.ts
+++ src/actors/thread.ts
@@ -1,10 +1,24 @@
 import { Vector, Color, Actor } from "excalibur";
 import { Ship } from "./ship";
 
+const colors = [
+  Color.Azure,
+  Color.Chartreuse,
+  Color.Magenta,
+  Color.Rose,
+  Color.Orange,
+  Color.Viridian,
+  Color.Violet,
+  Color.Vermillion,
+  Color.Cyan,
+  Color.Yellow
+];
+
 export class SpaceThread extends Actor {
   private startPoint: Vector;
   private ship: Ship;
   private endPoint: Vector | null;
+  private threadColor: string;
 
   constructor(ship: Ship, startPoint: Vector) {
     super({
@@ -13,6 +27,9 @@ export class SpaceThread extends Actor {
       height: 10
     });
 
+    const colorIdx = Math.floor(Math.random() * colors.length);
+    this.threadColor = colors[colorIdx].toString();
+
     this.ship = ship;
     this.startPoint = startPoint;
     this.endPoint = null;
@@ -20,17 +37,24 @@ export class SpaceThread extends Actor {
 
   draw(ctx: CanvasRenderingContext2D, delta: number) {
     ctx.beginPath();
-    ctx.strokeStyle = Color.Magenta.toString();
+    ctx.strokeStyle = this.threadColor;
     ctx.lineWidth = 4;
     ctx.lineJoin = "round";
     ctx.lineCap = "round";
 
+    // Move to the start point
     ctx.moveTo(this.startPoint.x, this.startPoint.y);
+
+    // Knot
+    ctx.arc(this.startPoint.x, this.startPoint.y, 5, 0, Math.PI * 2);
+
     if (this.endPoint) {
       ctx.lineTo(this.endPoint.x, this.endPoint.y);
+      ctx.arc(this.endPoint.x, this.endPoint.y, 5, 0, Math.PI * 2);
     } else {
       const { x, y } = this.ship.getThreadAttachPoint();
       ctx.lineTo(x, y);
+      ctx.arc(x, y, 2, 0, Math.PI * 2);
     }
     ctx.stroke();
     ctx.closePath();
